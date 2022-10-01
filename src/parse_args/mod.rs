15597dbe6942ec053541398ce0e9dedc10a4d3ea
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2022-10-01 17:43:20 +0000

    chore: Update clap to v4

--- src/parse_args/mod.rs
+++ src/parse_args/mod.rs
@@ -3,29 +3,29 @@ use std::process;
 use clap::Parser;
 use tracing::error;
 
-#[derive(Parser, Debug, Clone)]
-#[clap(about, version, author)]
-
+#[derive(Parser, Debug, Clone, Copy)]
+// #[command(help_template = FULL_TEMPLATE)]
+#[command(version, about)]
 pub struct CliArgs {
     /// Docker update interval in ms, minimum 1, reccomended 500+
     #[clap(short = 'd', value_name = "ms", default_value_t = 1000)]
     pub docker_interval: u32,
 
-    /// Don't draw gui - for debugging - mostly pointless
-    #[clap(short = 'g')]
-    pub gui: bool,
-
     /// Remove timestamps from Docker logs
     #[clap(short = 't')]
     pub timestamp: bool,
+
+    /// Attempt to colorize the logs
+    #[clap(short = 'c', conflicts_with = "raw")]
+    pub color: bool,
 
     /// Show raw logs, default is to remove ansi formatting
     #[clap(short = 'r', conflicts_with = "color")]
     pub raw: bool,
 
-    /// Attempt to colorize the logs
-    #[clap(short = 'c', conflicts_with = "raw")]
-    pub color: bool,
+    /// Don't draw gui - for debugging - mostly pointless
+    #[clap(short = 'g')]
+    pub gui: bool,
 }
 
 impl CliArgs {
