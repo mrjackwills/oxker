71ae53cc82d7f073f01c007b8091474586f08f5b
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2022-10-16 01:53:29 +0000

    fix: create_release.sh closes sed

--- create_release.sh
+++ create_release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # rust create_release
-# v0.1.1
+# v0.1.2
 
 STAR_LINE='****************************************'
 CWD=$(pwd)
@@ -108,8 +108,8 @@ update_release_body_and_changelog () {
 	sed -i -E "s=(\s)\[([0-9a-f]{8})([0-9a-f]{32})\]= [\2](${GIT_REPO_URL}/commit/\2\3),=g" ./CHANGELOG.md
 
 	# Update changelog to add links to closed issues - comma included!
-	# "closes [#1]," -> "closes [#1](https:/www.../issues/1),""
-	sed -i -r -E "s=closes \[#([0-9]+)\],=closes [#\1](${GIT_REPO_URL}/issues/\1),=g" ./CHANGELOG.md
+	# "closes #1," -> "closes [#1](https:/www.../issues/1),""
+	sed -i -r -E "s=closes \#([0-9]+)\,=closes [#\1](${GIT_REPO_URL}/issues/\1),=g" ./CHANGELOG.md
 }
 
 # update version in cargo.toml, to match selected current version
